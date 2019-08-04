ef2ab6a4a845260f01e5009d8e79331cbffb129e
johnathantng <johnathan.tng@gmail.com>
2019-08-04 04:51:34 -0700

    added username and password input fields

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import { Input } from './common';
+import { CardSection, Input } from './common';
 
 const LoginForm = () => {
 	return (
-		<Input
-			label="username"
-			placeholder="username or email"
-		/>
+		<View>
+			<CardSection>
+			<Input
+				label="username"
+				placeholder="username or email"
+			/>
+			</CardSection>
+			<CardSection>
+			<Input
+				label="password"
+				placeholder="password"
+				secureTextEntry
+			/>
+			</CardSection>
+		</View>
 	);
 };
 
