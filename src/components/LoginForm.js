6b837729be2b40fa70c0182402f04974ea2b9d0c
johnathantng <johnathan.tng@gmail.com>
2019-08-04 19:50:32 -0700

    added state for login username text

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 import { CardSection, Input, Button } from './common';
 
 const LoginForm = () => {
+	const [loginUser, setLoginUser] = useState('');
+
+	onUserChange = (text) => {
+		setLoginUser(text);
+		console.log({loginUser});
+	};
+
 	return (
 		<View>
 			<CardSection>
 				<Input
 					label="username"
 					placeholder="username or email"
+					value={loginUser}
+					onChangeText={(text) => onUserChange(text)}
 				/>
 			</CardSection>
 
