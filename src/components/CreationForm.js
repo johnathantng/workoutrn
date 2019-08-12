ee185d75e2ab061fba1899944f7e85c9f39d99ac
johnathantng <johnathan.tng@gmail.com>
2019-08-12 07:16:37 -0700

    added strict input - currently working on creation form

--- src/components/CreationForm.js
+++ src/components/CreationForm.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 
-import { CardSection, Input } from './common';
+import { CardSection, StrictInput } from './common';
 
 const CreationForm = () => {
 	return (
 		<View>
 			<CardSection>
-				<Input 
-					label="weight" 
-					placeholder="enter your weight"
+				<StrictInput 
+					label="age"
+					placeholder="enter your age"
+					maxLength={3}
+					keyboardType="number-pad"
 				/>
+			</CardSection>
+			<CardSection>
+
+			</CardSection>
+			<CardSection>
+
 			</CardSection>
 		</View>
 	);
+};
+
+const styles = {
+	inputStyle: {
+		color: '#000',
+		paddingRight: 5,
+		paddingLeft: 5,
+		fontSize: 18,
+		lineHeight: 23,
+		flex: 2
+	},
+	labelStyle: {
+		fontSize: 18,
+		paddingLeft: 20,
+		flex: 1,
+		color: '#352e30'
+	},
+	containerStyle: {
+		height: 40,
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center'
+	}
 };
 
 export default CreationForm;
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,6 +1,7 @@
 export * from './Button';
 export * from './Card';
 export * from './CardSection';
+export * from './StrictInput';
 export * from './Input';
 export * from './Spinner';
 export * from './ScreenSpinner';
