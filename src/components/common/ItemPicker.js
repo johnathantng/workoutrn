c8a9dee32a7faa208cd805fdcbc96815c00cd782
johnathantng <johnathan.tng@gmail.com>
2019-08-13 16:32:54 -0700

    added boilerplate code for itempicker.js

---
+++ src/components/common/ItemPicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Picker } from 'react-native';
+
+const ItemPicker = () => {
+	return (
+	);
+};
+
+export { ItemPicker };
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,6 +1,7 @@
 export * from './Button';
 export * from './Card';
 export * from './CardSection';
+export * from './ItemPicker';
 export * from './StrictInput';
 export * from './Input';
 export * from './Spinner';
