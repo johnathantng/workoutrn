359a911c5ec6b4b225a9e80b0bf83df2b5450956
johnathantng <johnathan.tng@gmail.com>
2019-08-15 21:05:52 -0700

    created WorkoutList component - need to move from Home to there

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,13 +17,37 @@ const Home = (props) => {
 				setUserName(user.data.username);
 				isLoading(false);
 			})
+			.catch(err => res.json('something went wrong'))
+		axios.get(`http://10.0.2.2:8685/profile/${props.user}/workouts`)
+			.then(res => {
+				console.log(res.data.length);
+				for (i = 0; i < res.data.length; i++) {
+					console.log(res.data[i]);
+					<Card>
+						<Text style={textHeaderStyle}> Name </Text>
+						<Text style={textStyle}> {res.data[i].} </Text>
+					</Card>
+				}
+			})
 	}, [])
+
+	renderHomeInfo = () => {
+		if (loading) {
+			return <ScreenSpinner />;
+		} else {
+			return (
+				<ScrollView style={styles.scrollViewStyle}>
+					<Text> {props.user} </Text>
+				</ScrollView>
+			);
+		}
+	};
 
 	return (
 		<View style={styles.containerStyle}>
-			<ScrollView style={styles.scrollViewStyle}>
-				<Text> {props.user} </Text>
-			</ScrollView>
+
+			{renderHomeInfo()}
+
 			<CardSection>
 				<Button onPress={() => Actions.workout({user: props.user, userName: userName})}> Add Workout </Button>
 			</CardSection>
@@ -38,6 +62,16 @@ const styles = {
 	},
 	scrollViewStyle: {
 		flex: 1
+	},
+		textHeaderStyle: {
+		textAlign: 'center',
+		fontSize: 18,
+		color: '#352e30'
+	},
+	textStyle: {
+		marginTop: 10,
+		marginBottom: 10,
+		textAlign: 'center'
 	}
 }
 
