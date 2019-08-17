6237ce305c5415d52564bad4ae1d2c1fe3d9d4a7
johnathantng <johnathan.tng@gmail.com>
2019-08-17 14:03:31 -0700

    added spinner to workoutlist

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -2,27 +2,35 @@ import React, { useState, useEffect } from 'react';
 import { ScrollView, Text } from 'react-native';
 import axios from 'axios';
 
-import { WorkoutCard } from './common';
+import { WorkoutCard, ScreenSpinner } from './common';
 
 const WorkoutList = (props) => {
 	const [workouts, setWorkouts] = useState([]);
+	const [loading, isLoading] = useState(false);
 
 	useEffect(() => {
+		isLoading(true);
 		axios.get(`http://10.0.2.2:8685/profile/${props.user}/workouts`)
-			.then(res => setWorkouts(res.data))
+			.then(res => {
+				setWorkouts(res.data);
+				isLoading(false);
+			})
 			.catch(err => console.log(err))
 	}, [])
 
 	renderWorkoutList = () => {
-		return workouts.map(data => {
-			console.log(data)
-			return <WorkoutCard 
-								key={data.workout_id} 
-								workoutName={data.workout} 
-								targetReps={data.target_reps}
-								targetSets={data.target_sets}
-							/>;
-		});
+		if (loading) {
+			return <ScreenSpinner />
+		} else {
+			return workouts.map(data => {
+				return <WorkoutCard 
+									key={data.workout_id} 
+									workoutName={data.workout} 
+									targetReps={data.target_reps}
+									targetSets={data.target_sets}
+								/>;
+			});
+		}
 	};
 
 	return (
