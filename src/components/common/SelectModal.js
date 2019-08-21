8dbc174d31747a4fbe6b96714393d0c731bf5aae
johnathantng <johnathan.tng@gmail.com>
2019-08-20 20:14:25 -0700

    added styling to SelectModal

--- src/components/common/SelectModal.js
+++ src/components/common/SelectModal.js
@@ -5,6 +5,8 @@ import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 const SelectModal = ({ visible, onPressEdit, onPressDelete }) => {
+	const { containerStyle, textStyle, cardSectionStyle } = styles;
+
 	return (
 		<Modal
 			visible={visible}
@@ -12,8 +14,8 @@ const SelectModal = ({ visible, onPressEdit, onPressDelete }) => {
 			animationType="slide"
 			onRequestClose={() => {}}
 		>
-			<View>
-				<CardSection>
+			<View style={containerStyle}>
+				<CardSection style={cardSectionStyle}>
 					<Button onPress={onPressEdit}> Edit </Button>
 					<Button onPress={onPressDelete}> Delete </Button>
 				</CardSection>
@@ -21,5 +23,23 @@ const SelectModal = ({ visible, onPressEdit, onPressDelete }) => {
 		</Modal>
 	);
 };
+
+const styles = {
+	cardSectionStyle: {
+		justifyContent: 'center'
+	},
+	textStyle: {
+		flex: 1,
+		fontSize: 18,
+		textAlign: 'center',
+		lineHeight: 40
+	},
+	containerStyle: {
+		backgroundColor: 'rgba(0, 0, 0, 0.75',
+		position: 'relative',
+		flex: 1,
+		justifyContent: 'center'
+	}
+}
 
 export { SelectModal };
