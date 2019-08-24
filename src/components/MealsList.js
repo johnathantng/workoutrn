b1b32ceeace5ad2c462553d7298113b5edae0701
johnathantng <johnathan.tng@gmail.com>
2019-08-24 03:52:21 -0700

    added MealsList - needs updating before usable

---
+++ src/components/MealsList.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import { ScrollView, Text } from 'react-native';
+import axios from 'axios';
+import { Actions } from 'react-native-router-flux';
+
+import { WorkoutCard, ScreenSpinner, SelectModal } from './common';
+
+const MealsList = (props) => {
+	const [meals, setMeals] = useState([]);
+	const [targetUserId, setTargetUserId] = useState('');
+	const [targetMeal, setTargetMeal] = useState('');
+	const [loading, isLoading] = useState(false);
+	const [modal, showModal] = useState(false);
+
+	useEffect(() => {
+		isLoading(true);
+		axios.get(`http://10.0.2.2:8685/profile/${props.user}/meals`)
+			.then(res => {
+				setWorkouts(res.data.reverse());
+				isLoading(false);
+			})
+			.catch(err => isLoading(false));
+	}, [])
+
+	onPressEdit = () => {
+		showModal(false);
+		axios.get(`http://10.0.2.2:8685/profile/${targetUserId}/meals/${targetMeal}`)
+			.then(res => Actions.edit({
+				user: props.user,
+				workout_id: res.data.workout_id,
+				workoutName: res.data.workout,
+				workoutType: res.data.type,
+				currentReps: res.data.current_reps,
+				currentSets: res.data.current_sets,
+				targetReps: res.data.target_reps,
+				targetSets: res.data.target_sets
+			}))
+			.catch(err => console.log(err))
+	}
+
+	onPressDelete = () => {
+		showModal(false);
+		axios.delete(`http://10.0.2.2:8685/profile/${targetUserId}/workouts/${targetWorkout}`)
+			.then(() => Actions.main({type: 'reset', user: props.user}))
+			.catch(err => console.log(err))
+	}
+
+	renderWorkoutList = () => {
+		if (loading) {
+			return <ScreenSpinner />
+		} else {
+			return workouts.map(data => {
+				return <WorkoutCard 
+									key={data.workout_id} 
+									workoutName={data.workout} 
+									targetReps={data.target_reps}
+									targetSets={data.target_sets}
+									currentReps={data.current_reps}
+									currentSets={data.current_sets}
+									onPress={() => Actions.edit({
+										user: props.user,
+										workout_id: data.workout_id, 
+										workoutName: data.workout,
+										workoutType: data.type,
+										currentReps: data.current_reps,
+										currentSets: data.current_sets,
+										targetReps: data.target_reps,
+										targetSets: data.target_sets
+									})}
+									onLongPress={() => {
+										showModal(true);
+										axios.get(`http://10.0.2.2:8685/profile/${props.user}/workouts/${data.workout_id}`)
+											.then(res => {
+												setTargetUserId(res.data.id);
+												setTargetWorkout(res.data.workout_id);
+											})
+											.catch(err => console.log(err))
+									}}
+								/>;
+			});
+		}
+	};
+
+
+
+	return (
+		<ScrollView>
+
+			<SelectModal 
+				visible={modal} 
+				onBackgroundPress={() => showModal(false)} 
+				onPressEdit={() => onPressEdit()}
+				onPressDelete={() => onPressDelete()}
+			/>
+
+			{renderWorkoutList()}
+
+		</ScrollView>
+	);
+};
+
+export default MealsList;
