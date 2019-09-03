48529dd578d508c72f45c025ec99684931992166
johnathantng <johnathan.tng@gmail.com>
2019-09-03 11:14:07 -0700

    removed console.log in event and also added a note

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,11 +5,11 @@ import NavBar from './NavBar';
 import UserContext from './LoginForm';
 
 const Event = (props) => {
-	console.log(props.user);
 	return (
 		<View style={styles.containerStyle}>
 			<ScrollView style={styles.scrollViewStyle}>
 				<Text> Event </Text>
+				{/* Create a list of weekdays in order to note your progress */}
 			</ScrollView>
 			<NavBar eventOpacity={0.7} user={props.user}/>
 		</View>
