04c629a2b42b87fcecd2217633bee6b7b27b9bd8
johnathantng <johnathan.tng@gmail.com>
2019-09-04 19:26:55 -0700

    created basic DayCard.js

--- src/components/common/DayCard.js
+++ src/components/common/DayCard.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { TouchableOpacity, View, Text } from 'react-native';
 
-const DayCard = () => {
+const DayCard = ({ dayName }) => {
+	const { containerStyle, subCategoriesStyle, textHeaderStyle, textStyle } = styles;
 	return (
-
+		<TouchableOpacity onPress={onPress} onLongPress={onLongPress}>
+			<View style={containerStyle}>
+				<Text style={textHeaderStyle}> {dayName} </Text>
+			</View>
+		</TouchableOpacity>
 	);
 }
+
+const styles = {
+	containerStyle: {
+		backgroundColor: '#fff',
+		borderWidth: 1,
+		borderRadius: 2,
+		borderColor: '#ddd',
+		borderBottomWidth: 0,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.1,
+		shadowRadius: 2,
+		elevation: 1,
+		marginLeft: 5,
+		marginRight: 5,
+		marginTop: 5,
+		marginBottom: 5
+	},
+	subCategoriesStyle: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginLeft: 20,
+		marginRight: 20
+	},
+	textHeaderStyle: {
+		textAlign: 'center',
+		fontSize: 18,
+		color: '#352e30'
+	},
+	textStyle: {
+		marginTop: 10,
+		marginBottom: 10,
+		textAlign: 'center'
+	}
+};
 
 export { DayCard };
