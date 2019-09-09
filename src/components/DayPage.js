4a71d47cb6150e7130b3c75ecca0af904013029f
johnathantng <johnathan.tng@gmail.com>
2019-09-08 20:30:05 -0400

    added heading into DayPage.js

--- src/components/DayPage.js
+++ src/components/DayPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const DayPage = () => {
+const DayPage = ({ dayName }) => {
     return (
         <View>
-            <Text> Test </Text>
+            <Text> Here is your summary for {dayName} </Text> 
         </View>
     );
 };
