9c9ef514c940ecd4cae162d873f8db5efbc31e33
johnathantng <johnathan.tng@gmail.com>
2019-09-11 08:54:59 -0400

    added a total calories for the DayPage.js

--- src/components/DayPage.js
+++ src/components/DayPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const DayPage = ({ dayName }) => {
+const DayPage = ({ dayName, totalCalories }) => {
     return (
         <View>
             <Text> Here is your summary for {dayName} </Text> 
+            <Text> Your total calories for {dayName} is {totalCalories} </Text>
         </View>
     );
 };
