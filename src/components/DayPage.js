5f883b7682e64ca2e63d8218b71af1a92c5d7939
johnathantng <johnathan.tng@gmail.com>
2019-09-20 18:54:16 -0400

    added a menu to daypage.js

--- src/components/DayPage.js
+++ src/components/DayPage.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import WorkoutList from './WorkoutList';
+import { Menu } from './common';
 
 const DayPage = ({ dayName, totalCalories }) => {
     return (
         <View>
             <Text style={styles.textStyle}> Here is your summary for {dayName} </Text> 
             <Text style={styles.textStyle}> Your total calories for {dayName} is {totalCalories} </Text>
-            <Text style={styles.textStyle}> Here are your exercises for today </Text>
+            <Text style={styles.textStyle}> Here are your exercises and meals for today </Text>
+            <Menu 
+                labelButtonOne="Workouts" 
+                labelButtonTwo="Meals"
+                //  onPressButtonOne={() => onPressWorkouts()}
+                //  onPressButtonTwo={() => onPressMeals()}
+            />
             <WorkoutList />
         </View>
     );
