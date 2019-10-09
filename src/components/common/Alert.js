0728310f0e430b72505825fbecff6eea2b47b96a
johnathantng <johnathan.tng@gmail.com>
2019-10-09 17:57:22 -0400

    created a placeholder for alert.js

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 
 const Alert = () => {
 	//alert for errors / server issues
+	<View>
+		<Text> Hello! This is an alert! </Text>
+	</View>
 }
 
 export { Alert };
